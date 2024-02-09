2b71736295cea76a56efe37b4a0d18cae4f03dfe
tsrhayar <tahasrhayar1@gmail.com>
2024-02-09 12:53:07 +0100

    add reset-password page and config gmail

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,91 +1,111 @@
 @php
-$customizerHidden = 'customizer-hide';
-$configData = Helper::appClasses();
+    $pageConfigs = ['myLayout' => 'blank'];
+    $customizerHidden = 'customizer-hide';
 @endphp
 
-@extends('layouts/blankLayout')
+@extends('layouts/layoutMaster')
 
-@section('title', 'Reset Password')
+@section('title', 'Reset Password Basic - Pages')
+
+@section('vendor-style')
+    <!-- Vendor -->
+    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/@form-validation/umd/styles/index.min.css') }}" />
+@endsection
 
 @section('page-style')
-<!-- Page -->
+    <!-- Page -->
+    <link rel="stylesheet" href="{{ asset('assets/vendor/css/pages/page-auth.css') }}">
+@endsection
+
+@section('vendor-script')
+    <script src="{{ asset('assets/vendor/libs/@form-validation/umd/bundle/popular.min.js') }}"></script>
+    <script src="{{ asset('assets/vendor/libs/@form-validation/umd/plugin-bootstrap5/index.min.js') }}"></script>
+    <script src="{{ asset('assets/vendor/libs/@form-validation/umd/plugin-auto-focus/index.min.js') }}"></script>
+@endsection
+
+@section('page-script')
+    <script src="{{ asset('assets/js/pages-auth.js') }}"></script>
 @endsection
 
 @section('content')
-<div class="authentication-wrapper authentication-cover authentication-bg">
-  <div class="authentication-inner row">
-    <!-- /Left Text -->
-    <div class="d-none d-lg-flex col-lg-7 p-0">
-      <div class="auth-cover-bg auth-cover-bg-color d-flex justify-content-center align-items-center">
-        <img src="{{ asset('assets/img/illustrations/auth-forgot-password-illustration-'.$configData['style'].'.png') }}" alt="auth-forgot-password-cover" class="img-fluid my-5 auth-illustration" data-app-light-img="illustrations/auth-forgot-password-illustration-light.png" data-app-dark-img="illustrations/auth-forgot-password-illustration-dark.png">
+    <div class="container-xxl">
+        <div class="authentication-wrapper authentication-basic container-p-y">
+            <div class="authentication-inner py-4">
+                <!-- Reset Password -->
+                <div class="card">
+                    <div class="card-body">
+                        <!-- Logo -->
+                        <div class="app-brand justify-content-center mb-4 mt-2">
+                            <a href="{{ url('/') }}" class="app-brand-link gap-2">
+                                <span class="app-brand-logo demo">@include('_partials.macros', ['height' => 20, 'withbg' => 'fill: #fff;'])</span>
+                                <span
+                                    class="app-brand-text demo text-body fw-bold ms-1">{{ config('variables.templateName') }}</span>
+                            </a>
+                        </div>
+                        <!-- /Logo -->
+                        <h4 class="mb-1 pt-2">Reset Password 🔒</h4>
+                        <p class="mb-4">for <span class="fw-medium">{{ Request()->email }}</span></p>
+                        <form id="formAuthentication" class="mb-3" action="{{ route('password.update') }}" method="POST">
+                            @csrf
+                            <input type="hidden" name="token" value="{{ $request->route('token') }}">
 
-        <img src="{{ asset('assets/img/illustrations/bg-shape-image-'.$configData['style'].'.png') }}" alt="auth-forgot-password-cover" class="platform-bg" data-app-light-img="illustrations/bg-shape-image-light.png" data-app-dark-img="illustrations/bg-shape-image-dark.png">
-      </div>
-    </div>
-    <!-- /Left Text -->
-    <!-- Reset Password -->
-    <div class="d-flex col-12 col-lg-5 align-items-center p-sm-5 p-4">
-      <div class="w-px-400 mx-auto">
-        <!-- Logo -->
-        <div class="app-brand mb-4">
-          <a href="{{url('/')}}" class="app-brand-link">
-            <span class="app-brand-logo demo">@include('_partials.macros',["height"=>20,"withbg"=>'fill: #fff;'])</span>
-          </a>
+                            <div class="mb-3 d-none">
+                                <label for="email" class="form-label">Email</label>
+                                <input type="email" class="form-control @error('email') is-invalid @enderror"
+                                    id="email" name="email" placeholder="john@example.com"
+                                    value="{{ Request()->email }}" readonly />
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <span class="fw-medium">{{ $message }}</span>
+                                    </span>
+                                @enderror
+                            </div>
+
+                            <div class="mb-3 form-password-toggle">
+                                <label class="form-label" for="password">New Password</label>
+                                <div class="input-group input-group-merge @error('password') is-invalid @enderror">
+                                    <input type="password" id="password"
+                                        class="form-control @error('password') is-invalid @enderror" name="password"
+                                        placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
+                                        aria-describedby="password" autofocus />
+                                    <span class="input-group-text cursor-pointer">
+                                        <i class="ti ti-eye-off"></i>
+                                    </span>
+                                </div>
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <span class="fw-medium">{{ $message }}</span>
+                                    </span>
+                                @enderror
+                            </div>
+                            <div class="mb-3 form-password-toggle">
+                                <label class="form-label" for="confirm-password">Confirm Password</label>
+                                <div class="input-group input-group-merge">
+                                    <input type="password" id="confirm-password" class="form-control"
+                                        name="password_confirmation"
+                                        placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
+                                        aria-describedby="password" />
+                                    <span class="input-group-text cursor-pointer">
+                                        <i class="ti ti-eye-off"></i>
+                                    </span>
+                                </div>
+                            </div>
+                            <button type="submit" class="btn btn-primary d-grid w-100 mb-3">
+                                Set new password
+                            </button>
+                            <div class="text-center">
+                                @if (Route::has('login'))
+                                    <a href="{{ route('login') }}">
+                                        <i class="ti ti-chevron-left scaleX-n1-rtl"></i>
+                                        Back to login
+                                    </a>
+                                @endif
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <!-- /Reset Password -->
+            </div>
         </div>
-        <!-- /Logo -->
-        <h3 class="mb-1">Reset Password 🔒</h3>
-        <form id="formAuthentication" class="mb-3" action="{{ route('password.update') }}" method="POST">
-          @csrf
-          <input type="hidden" name="token" value="{{ $request->route('token') }}">
-
-          <div class="mb-3">
-            <label for="email" class="form-label">Email</label>
-            <input type="email" class="form-control @error('email') is-invalid @enderror" id="email" name="email" placeholder="john@example.com" value="{{Request()->email}}" readonly />
-            @error('email')
-            <span class="invalid-feedback" role="alert">
-              <span class="fw-medium">{{ $message }}</span>
-            </span>
-            @enderror
-          </div>
-
-          <div class="mb-3 form-password-toggle">
-            <label class="form-label" for="password">New Password</label>
-            <div class="input-group input-group-merge @error('password') is-invalid @enderror">
-              <input type="password" id="password" class="form-control @error('password') is-invalid @enderror" name="password" placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;" aria-describedby="password" autofocus />
-              <span class="input-group-text cursor-pointer">
-                <i class="ti ti-eye-off"></i>
-              </span>
-            </div>
-            @error('password')
-            <span class="invalid-feedback" role="alert">
-              <span class="fw-medium">{{ $message }}</span>
-            </span>
-            @enderror
-          </div>
-          <div class="mb-3 form-password-toggle">
-            <label class="form-label" for="confirm-password">Confirm Password</label>
-            <div class="input-group input-group-merge">
-              <input type="password" id="confirm-password" class="form-control" name="password_confirmation" placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;" aria-describedby="password" />
-              <span class="input-group-text cursor-pointer">
-                <i class="ti ti-eye-off"></i>
-              </span>
-            </div>
-          </div>
-          <button type="submit" class="btn btn-primary d-grid w-100 mb-3">
-            Set new password
-          </button>
-          <div class="text-center">
-            @if (Route::has('login'))
-            <a href="{{ route('login') }}">
-              <i class="ti ti-chevron-left scaleX-n1-rtl"></i>
-              Back to login
-            </a>
-            @endif
-          </div>
-        </form>
-      </div>
     </div>
-    <!-- /Reset Password -->
-  </div>
-</div>
 @endsection
